8562a4081225f6fd51606e8379b80addb833526b
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-04-29 17:46:26 -0300

    feat: add font open sans

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&family=Open+Sans&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
