ffd6d64d57aec5b260244e1fbfab6c4bcf09b197
Luiz Gustavo <luizgustavofernandes16@gmail.com>
2022-05-06 09:46:22 -0300

    setup-mocks

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -1,1 +1,35 @@
 import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => {
+    return {
+      t: (str: any) => str,
+      i18n: {
+        changeLanguage: () => new Promise(() => {}),
+      },
+    };
+  },
+}));
+
+export const mockToastError = jest.fn();
+export const mockToastSuccess = jest.fn();
+
+jest.mock('react-toastify', () => {
+  const actual = jest.requireActual('react-toastify');
+  Object.assign(actual, {
+    toast: {
+      error: mockToastError,
+      success: mockToastSuccess,
+    },
+  });
+  return actual;
+});
+
+export const replaceMock = jest.fn();
+
+delete window.location;
+window.location = { replace: replaceMock };
+
+afterEach(() => {
+  replaceMock.mockClear();
+});
